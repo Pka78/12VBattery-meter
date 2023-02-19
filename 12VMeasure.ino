c8c8a545c956c7095d8ce89fa3f46b9958a51628
Pka78 <petri@kajander.info>
2023-02-19 11:34:49 +0200

    Update 12VMeasure.ino

--- 12VMeasure.ino
+++ 12VMeasure.ino
@@ -1,77 +1,63 @@
 #include <BLEDevice.h>
-#include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
-const int voltagePin = 35; // analog input pin for voltage measurement
-const int ledPin = 2; // pin for the built-in LED
-const float vDivider = 0.176; // voltage divider factor
-const int reportInterval = 60000; // report interval in milliseconds
 
-BLEServer *pServer = NULL;
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+float voltage;
 
-class MyServerCallbacks : public BLEServerCallbacks {
+class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
-    }
+      deviceConnected = true;
+    };
+
     void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
     }
 };
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
 
-  // Create the BLE Device
-  BLEDevice::init("12V Battery Meter");
-
-  // Create the BLE Server
+  // Initialize BLE
+  BLEDevice::init("MyDevice");
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
 
-  // Create the BLE Service
-  BLEService *pService = pServer->createService("0x180F");
+  // Create the service
+  BLEService *pService = pServer->createService(BLEUUID(SERVICE_UUID));
 
-  // Create the BLE Characteristic for voltage measurement
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         BLEUUID("2A19"),
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_NOTIFY
-                                       );
-
-  pCharacteristic->addDescriptor(new BLE2902());
+  // Create the characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      BLEUUID(CHARACTERISTIC_UUID),
+                      BLECharacteristic::PROPERTY_READ |
+                      BLECharacteristic::PROPERTY_NOTIFY);
 
   // Start the service
   pService->start();
 
   // Start advertising
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->addServiceUUID(pService->getUUID());
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);
-  pAdvertising->setMinPreferred(0x12);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
   BLEDevice::startAdvertising();
-
 }
 
 void loop() {
-  float voltage = 0.0;
-  int numSamples = 100;
-  for (int i = 0; i < numSamples; i++) {
-    voltage += analogRead(voltagePin);
-    delay(1);
+  // Measure voltage on ADC pin
+  voltage = (float) analogRead(A0) * 0.0049;  // conversion factor for 12V range
+
+  // Update characteristic value
+  if (deviceConnected) {
+    String data = String(voltage, 2);
+    pCharacteristic->setValue(data.c_str());
+    pCharacteristic->notify();
   }
-  voltage /= numSamples;
-  voltage = voltage * vDivider;
-  Serial.printf("Voltage: %.2fV\n", voltage);
 
-  // blink the LED
-  digitalWrite(ledPin, HIGH);
-  delay(100);
-  digitalWrite(ledPin, LOW);
-
-  // Update the BLE Characteristic with the current voltage
-  String voltageStr = String(voltage, 2);
-  pServer->updateCharacteristicValue(pCharacteristic->getHandle(), voltageStr.c_str(), voltageStr.length());
-
-  // Wait for the next report interval
-  delay(reportInterval);
+  delay(1000);  // delay for 1 second before taking next reading
 }
+
